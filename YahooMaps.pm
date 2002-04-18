dc2b63ac1815d5c2cff6f6d650a1de35f8c6cd0d
Gabor Cselle <gaborcselle@yahoo.de>
2002-04-18 15:51:01 +0000

    Import of CSELLEG/WWW-YahooMaps-0.2 from CPAN.
    gitpan-cpan-distribution: WWW-YahooMaps
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         CSELLEG/WWW-YahooMaps-0.2.tar.gz
    gitpan-cpan-author:       CSELLEG
    gitpan-cpan-maturity:     released

--- YahooMaps.pm
+++ YahooMaps.pm
@@ -8,7 +8,7 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 
 use URI::Escape;
 
@@ -47,7 +47,16 @@ sub hashreftolink {
         $rh_addr->{language} = &ukcheck($rh_addr->{language});
         $rh_addr->{country} = &ukcheck($rh_addr->{country});
 
+	#special treatment for canada and US
+	if ($rh_addr->{country} eq "ca") {
+	    $rh_addr->{language} = "ca";
+	} elsif ($rh_addr->{country} eq "us") {
+	    $rh_addr->{language} = "us";
+	}
+
+	#take care of US dot-related issues
 	my $langdot = '';
+
 	if ($rh_addr->{language} eq "us") {
 	    $rh_addr->{language} = "";
 	} else {
@@ -56,8 +65,14 @@ sub hashreftolink {
 
         my $url = "";
         $url  = "http://" . $rh_addr->{language} . $langdot . "maps.yahoo.com";
-	$url .= "/py/lg:" . $rh_addr->{language};
-	$url .= "/lc:" . $rh_addr->{language};
+
+	$url .= "/py";
+
+	if ($rh_addr->{country} ne "ca") {
+	    $url .= "/lg:" . $rh_addr->{language};
+	    $url .= "/lc:" . $rh_addr->{language};
+	}
+
 	$url .= "/maps.py?BFCat=&Pyt=Tmap&newFL=";    
 
         # street
@@ -125,6 +140,7 @@ sub testcountry {
 	my %ctys = (
 		    "at" => 1,
 		    "be" => 1,
+		    "ca" => 1,
 		    "ch" => 1,
 		    "de" => 1,
 		    "es" => 1,
@@ -181,7 +197,7 @@ WWW::YahooMaps - Create links to Yahoo! Maps
  }
 
  #second method: ADDRESS INFO IN STRING
- #seperators can be ";" or newline "\n"
+ #separators can be ";" or newline "\n"
  #street should come before city
  #pass an additional 1 at the end if you want to pass city first
  if (my $url = WWW::YahooMaps::stringtolink("us","us","101 Morris Street; Sebastopol, CA 95472")){
@@ -205,13 +221,31 @@ url3 http://fr.maps.yahoo.com/py/lg:fr/lc:fr/maps.py?BFCat=&Pyt=Tmap&newFL=&addr
 
 =head1 DESCRIPTION
 
-Pass a reference to a hash to hashreftolink() and get a link to the corresponding Yahoo! map.
-Or, if you have a ready-string with the adress info, pass it to stringtolink() - see restrictions on $string above.
+=head2 Methods
 
-They offer maps for several countries - use the domain name for the {country} key. Currently available countries are: 
+=head3 hashreftolink()
+
+Pass a reference to a hash to hashreftolink() and get a link to the corresponding Yahoo! map.
+
+The keys of the hash are I<street> (optional), I<city>, I<country> and I<language>. The values of the keys country and language must be valid ISO 3166 two letter codes, and can be tested by the functions B<testcountry()> and B<testlanguage()> respectively.
+
+The key, value pair I<street> is optional, however adding it leads to a much more accurate map.
+
+The value of city can be either a postal code or a city name, however adding both produces much more accurate map links. If a city name is ambiguous, for example there are multiple cities with that name, the link will lead to a page with links to the various cities of that name.
+
+=head3 stringtolink()
+
+Or, if you have a ready-made string with the adress info, pass it to stringtolink().
+
+The parameters are I<country>, I<language>, I<string> and I<swap>. In string, street and city should be separated by a semicolon ";" or a newline "\n". Since street is optional, you can also pass "; Paris" or "\n Zurich". Pass swap = 1 if you have a string where the city comes first, address second, e.g.: "New York, NY; Wall Street".
+
+=head2 Countries
+
+Yahoo offers maps for several countries - use the domain name (ISO 3166 2-letter code) for the {country} key. Currently available countries are: 
 
  - Austria (at)
  - Belgium (be)
+ - Canada (ca) - only in combination with language = "us"
  - France (fr)
  - Germany (de)
  - Italy (it)
@@ -220,18 +254,28 @@ They offer maps for several countries - use the domain name for the {country} ke
  - Portugal (pt)
  - Spain (es)
  - Switzerland (ch)
- - U.S.A. (us)
+ - U.S.A. (us) - only in combination with language = "us"
  - United Kingdom (gb/uk)
 
-Note that you can display maps for Germany in French, by passing "fr" as {language} and "de" as {country}.
-Supported languages are: 
+=head3 testcountry()
 
- - American English (us)
+Use testcountry() to determine which countries are available, e.g.: testcountry("it") => returns 1, testcountry("tv") => returns 0.
+
+=head2 Languages
+
+Note that you can display maps for Germany in French, by passing "fr" as {language} and "de" as {country}.
+Supported languages are (again, use ISO 3166 codes for language): 
+
+ - American English (us) - only in combination with country = "us"
  - British English (uk)
  - French (fr) 
  - German (de)
  - Italian (it)
  - Spanish (es)
+
+=head3 testlang()
+
+Use testlang() to determine which languages are available, e.g.: testlang("es") => returns 1, testlang("dk") => returns 0.
 
 The language also determines which Yahoo server is used - the servers for language="us" are in the US, while servers for all others are in Europe.
 
@@ -248,6 +292,13 @@ Gabor Cselle, gaborcselle@yahoo.de
 =head1 THANKS TO
 
 Ed Freyfogle, who wrote the original version.
+
+=head1 REVISIONS
+
+0.1 : first version
+
+0.2 : added Canada to countries, forces language="us" for US/CA, added docs for testcountry() and testlang()
+
 
 =head1 SEE ALSO
 
